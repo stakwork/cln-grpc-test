24617bf91ef41f1832a2a4df80d2ba4969f97457
irriden <irriden@proton.me>
2023-08-10 00:01:13 +0000

    configure also via .env file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod cln;
 mod utils;
 
 use clap::Parser;
+use dotenv::dotenv;
 use std::str::FromStr;
 
 /// cln-grpc playground
@@ -9,49 +10,50 @@ use std::str::FromStr;
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// eg. http://127.0.0.1
-    #[arg(short, long, default_value_t = String::from_str("http://127.0.0.1").unwrap())]
+    #[arg(short, long, env, default_value_t = String::from_str("http://127.0.0.1").unwrap())]
     url: String,
 
     /// eg. 10010
-    #[arg(short, long, default_value_t = String::from_str("10010").unwrap())]
+    #[arg(short, long, env, default_value_t = String::from_str("10010").unwrap())]
     nport: String,
 
     /// eg info
-    #[arg(short, long, default_value_t = String::from_str("info").unwrap())]
+    #[arg(short, long, env, default_value_t = String::from_str("info").unwrap())]
     command: String,
 
     /// eg 5
-    #[arg(short, long, default_value_t = 5u64)]
+    #[arg(short, long, env, default_value_t = 5u64)]
     amount_sat: u64,
 
     /// eg 02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0
-    #[arg(short, long, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
+    #[arg(short, long, env, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
     destination: String,
 
     /// eg 02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0
-    #[arg(short, long, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
+    #[arg(short, long, env, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
     lhpk: String,
 
     /// eg 5917632481235
-    #[arg(short, long, default_value_t = 5917632481235)]
+    #[arg(short, long, env, default_value_t = 5917632481235)]
     scid: u64,
 
     /// eg 1000
-    #[arg(short, long, default_value_t = 1000)]
+    #[arg(short, long, env, default_value_t = 1000)]
     base: u64,
 
     /// eg 40
-    #[arg(short, long, default_value_t = 40)]
+    #[arg(short, long, env, default_value_t = 40)]
     expirydelta: u32,
 
     /// eg 1
-    #[arg(short, long, default_value_t = 1)]
+    #[arg(short, long, env, default_value_t = 1)]
     prop: u32,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
+    dotenv().ok();
     let args = Args::parse();
     let url = args.url;
     let port = args.nport;
