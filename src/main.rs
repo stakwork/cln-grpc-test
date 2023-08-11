932444a8a19147bec1439943423a454845b9e050
irriden <irriden@proton.me>
2023-08-11 17:44:08 +0000

    add subcommand feature, getinfo keysend

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod cln;
 mod utils;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use dotenv::dotenv;
 use std::str::FromStr;
 use utils::to_cln;
@@ -23,41 +23,48 @@ struct Args {
     #[arg(short, long, env, default_value_t = String::from_str("10010").unwrap())]
     nport: String,
 
-    /// eg info
-    #[arg(short, long, env, default_value_t = String::from_str("getinfo").unwrap())]
-    command: String,
-
     /// eg /home/sphinx
     #[arg(long, env, default_value_t = String::from_str(".").unwrap())]
     creds: String,
 
-    /// eg 5
-    #[arg(short, long, env, default_value_t = 5u64)]
-    amount_sat: u64,
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    /// eg 02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0
-    #[arg(short, long, env, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
-    destination: String,
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// getinfo
+    Getinfo,
+    /// keysend
+    Keysend {
+        /// eg 5
+        #[arg(short, long, env, default_value_t = 5u64)]
+        amount_sat: u64,
 
-    /// eg 02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0
-    #[arg(short, long, env, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
-    lhpk: String,
+        /// eg 02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0
+        #[arg(short, long, env, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
+        destination: String,
 
-    /// eg 5917632481235
-    #[arg(short, long, env, default_value_t = 5917632481235)]
-    scid: u64,
+        /// eg 02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0
+        #[arg(short, long, env, default_value_t = String::from_str("02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0").unwrap())]
+        lhpk: String,
 
-    /// eg 1000
-    #[arg(short, long, env, default_value_t = 1000)]
-    base: u64,
+        /// eg 5917632481235
+        #[arg(short, long, env, default_value_t = 5917632481235)]
+        scid: u64,
 
-    /// eg 40
-    #[arg(short, long, env, default_value_t = 40)]
-    expirydelta: u32,
+        /// eg 1000
+        #[arg(short, long, env, default_value_t = 1000)]
+        base: u64,
 
-    /// eg 1
-    #[arg(short, long, env, default_value_t = 1)]
-    prop: u32,
+        /// eg 40
+        #[arg(short, long, env, default_value_t = 40)]
+        expirydelta: u32,
+
+        /// eg 1
+        #[arg(short, long, env, default_value_t = 1)]
+        prop: u32,
+    },
 }
 
 #[tokio::main]
@@ -72,26 +79,38 @@ async fn main() -> anyhow::Result<()> {
     let command = args.command;
     let creds_dir = args.creds;
 
-    // keysend arguments
-    let pk = args.destination;
-    let amt_msat = args.amount_sat * 1000;
-    let lhpk = args.lhpk;
-    let scid = to_cln(args.scid);
-    let feebase = args.base;
-    let expirydelta = args.expirydelta;
-    let feeprop = args.prop;
-
     let creds = utils::collect_creds(&creds_dir).await?;
     let mut client = cln::ClnRPC::try_new(&url, &port, &creds, 50).await?;
 
-    if &command == "getinfo" {
-        let info = client.get_info().await?;
-        println!("INFO {:?}", info);
-    } else if &command == "keysend" {
-        let hm = client
-            .keysend_with_route_hint(&pk, amt_msat, &lhpk, &scid, feebase, expirydelta, feeprop)
-            .await?;
-        println!("HM {:?}", hm);
+    match command {
+        Commands::Getinfo => {
+            let info = client.get_info().await?;
+            println!("INFO {:?}", info);
+        }
+        Commands::Keysend {
+            destination,
+            amount_sat,
+            lhpk,
+            scid,
+            base,
+            expirydelta,
+            prop,
+        } => {
+            let amt_msat = amount_sat * 1000;
+            let scid = to_cln(scid);
+            let hm = client
+                .keysend_with_route_hint(
+                    &destination,
+                    amt_msat,
+                    &lhpk,
+                    &scid,
+                    base,
+                    expirydelta,
+                    prop,
+                )
+                .await?;
+            println!("HM {:?}", hm);
+        }
     }
     Ok(())
 }
