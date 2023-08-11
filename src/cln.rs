ee9dacdf29e4b1c6464f002e7ef6aff23fb40758
irriden <irriden@proton.me>
2023-08-11 23:16:06 +0000

    successful keysend game b 1 -> game b 3 -> proxy node

--- src/cln.rs
+++ src/cln.rs
@@ -69,31 +69,64 @@ impl ClnRPC {
 
     pub async fn keysend_with_route_hint(
         &mut self,
-        id: &str,
-        amt: u64,
-        last_hop_id: &str,
-        scid: &str,
-        feebase: u64,
-        expirydelta: u32,
-        feeprop: u32,
+        _id: &str,
+        _amt: u64,
+        _last_hop_id: &str,
+        _scid: &str,
+        _feebase: u64,
+        _expirydelta: u32,
+        _feeprop: u32,
     ) -> Result<pb::KeysendResponse> {
-        let destination = hex::decode(id)?;
         let mut routehints = pb::RoutehintList { hints: vec![] };
-        let mut hint1 = pb::Routehint { hops: vec![] };
-        let hop1 = pb::RouteHop {
-            id: hex::decode(last_hop_id)?,
-            short_channel_id: scid.to_string(),
-            feebase: Some(_amount(feebase)),
-            expirydelta,
-            feeprop,
+        let mut hint0 = pb::Routehint { hops: vec![] };
+
+        // remy
+        //let id = "02970cbacbbd9871d532f08460612a0d6fd2e78ac2b95bd83f7048c7a14a9113e4";
+        //let amt_msat = 5000;
+        //let destination = hex::decode(id)?;
+
+        // deran
+        let id = "02e2b4a7836574e9504e86131ddb56f105feba1ebef2151684ccfbabf0114a1c77";
+        let amt_msat = 123000;
+        let destination = hex::decode(id)?;
+
+        // game b 1 -> game b 3
+        let gameb_1_id = "023d70f2f76d283c6c4e58109ee3a2816eb9d8feb40b23d62469060a2b2867b77f";
+        let gameb_1_scid = "750452x1749x1";
+        let gameb_1_feebase = 0;
+        let gameb_1_delta = 100;
+        let gameb_1_feeprop = 3000;
+        let hop0 = pb::RouteHop {
+            id: hex::decode(gameb_1_id)?,
+            short_channel_id: gameb_1_scid.to_string(),
+            feebase: Some(_amount(gameb_1_feebase)),
+            expirydelta: gameb_1_delta,
+            feeprop: gameb_1_feeprop,
         };
-        hint1.hops.push(hop1);
-        routehints.hints.push(hint1);
+
+        // game b 3 -> deran
+        let gameb_3_id = "02736e7dad83d7205826649fc17db672ce08f8e87a2b47c7785ccbf79f24e91db0";
+        let gameb_3_scid = to_cln(1099539677185);
+        let gameb_3_feebase = 0;
+        let gameb_3_delta = 0;
+        let gameb_3_feeprop = 0;
+        let hop1 = pb::RouteHop {
+            id: hex::decode(gameb_3_id)?,
+            short_channel_id: gameb_3_scid,
+            feebase: Some(_amount(gameb_3_feebase)),
+            expirydelta: gameb_3_delta,
+            feeprop: gameb_3_feeprop,
+        };
+
+
+        hint0.hops.push(hop0);
+        hint0.hops.push(hop1);
+        routehints.hints.push(hint0);
         let response = self
             .client
             .key_send(pb::KeysendRequest {
                 destination,
-                amount_msat: Some(_amount(amt)),
+                amount_msat: Some(_amount(amt_msat)),
                 routehints: Some(routehints),
                 ..Default::default()
             })
